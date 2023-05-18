0c47f7ec565cbfa44ff88ab7c9ce79ea8354116b
douglasfcsoares <douglasfcsoares@gmail.com>
2023-05-18 16:03:29 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/felixdevelopment/imc_challenge_dio/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.felixdevelopment.imc_challenge_dio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/imc/imc_challenge_dio.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Digite seu peso em Kg (Ex.: 82.5): ');
+  double peso = double.parse(stdin.readLineSync().toString());
+
+  stdout.write('Digite sua altura em metros (Ex.: 1.80): ');
+  double altura = double.parse(stdin.readLineSync().toString());
+
+  double imc = peso / (altura * altura);
+
+  print('Seu IMC é ${imc.toStringAsFixed(2)}');
+
+  if (imc < 16) {
+    print('Magreza grave');
+  } else if (imc < 17) {
+    print('Magreza moderada');
+  } else if (imc < 18.5) {
+    print('Magreza leve');
+  } else if (imc < 25) {
+    print('Saudável');
+  } else if (imc < 30) {
+    print('Sobrepeso');
+  } else if (imc < 35) {
+    print('Obesidade – GRAU I');
+  } else if (imc < 40) {
+    print('Obesidade – GRAU II | Severa');
+  } else {
+    print('Obesidade – GRAU III | Mórbida');
+  }
+}
